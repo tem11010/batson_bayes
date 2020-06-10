e8dc6f80dee739e494cae9b978c0c73da00a0f2a
xm419 <xiaomeng.li@uconn.edu>
2020-06-09 22:13:04 -0400

    find an easier way to get the strike_seq

--- prototype/extract_strike_seq.R
+++ prototype/extract_strike_seq.R
@@ -8,25 +8,11 @@ library(dplyr)
 dat0 <- readRDS("jury_data_cleaned.RDS")
 
 extract_seq <- function(dat){
-  
-  dat$strike_num1 <- as.numeric(dat$strike_num)
-  skipped <- as.numeric(union(setdiff(unique(dat$strike_num),seq(min(as.numeric(dat$strike_num)),max(as.numeric(dat$strike_num)))),setdiff(seq(min(as.numeric(dat$strike_num)),max(as.numeric(dat$strike_num))),unique(dat$strike_num))))
-  if(length(skipped)>0){
-    for(i in 1:length(skipped)){
-      dat$strike_num1[which(dat$strike_num1>skipped[i])] = dat$strike_num1[which(dat$strike_num1>skipped[i])]-1
-    }
-  }
-  ### find number of strikes by both PP and P
-  
-  n0 = length(dat$strike_num1)-sum(dat$strike_num1==0)-length(unique(dat$strike_num1))+1
-  idx = intersect(which(dat$strike_num1<=n0),which(dat$strike_num1>0))
-  
-  ### find the second index of duplicated strike numbers
-  second_index = intersect(which(duplicated(dat$strike_num1)),which(dat$strike_num1>0))
-  
-  dat$strike_seq[idx] = as.numeric(dat$strike_num1[idx])*2-1
-  dat$strike_seq[second_index] = as.numeric(dat$strike_num1[second_index])*2
-  dat$strike_seq[which(as.numeric(dat$strike_num1)>n0)] = as.numeric(dat$strike_num1[which(as.numeric(dat$strike_num1)>n0)])+n0
+  dat$strike_num <- as.numeric(dat$strike_num)
+  dat = dat[order(dat$strike_num),]
+  min0 <- min(dat$strike_num[which(dat$strike_num>0)])
+  legth <- length(dat$strike_num[which(dat$strike_num>0)])
+  dat$strike_seq[which(dat$strike_num>0)] <- seq(from=min0,to = min0+legth-1,by = 1)
   return(dat)
 }
 
@@ -35,5 +21,4 @@ test_l <- lapply(1:length(dat1), function(x) extract_seq(dat1[[x]]))
 dat1 = do.call(rbind.data.frame,test_l)
 
 saveRDS(dat1, file = "jury_data_cleaned_new.rds")
-
 

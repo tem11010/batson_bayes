cf518fa4f394c9206f74ba4cf3c7e6707f26c825
Sachin S. Pandya <sachin.pandya@uconn.edu>
2021-11-08 17:56:11 -0500

    minor revision and addition to R file re: coverage rate simulation

--- analysis/R/coverageplot1.R
+++ analysis/R/coverageplot1.R
@@ -60,3 +60,30 @@ p95 <- ggplot()+
         legend.text = element_text(size = 14))
 
 #p95
+
+
+#### dino plot #####
+ggplot()+
+  geom_tile(data = sims, aes(x = d, y = d_h,
+                             fill = C80),
+            color="black")+
+  facet_grid(h~a)+
+  scale_fill_gradient2(expression(C80),
+                       low = "white", 
+                       high = "blue",
+                       breaks = c(0,0.2,0.4,0.6,0.8,0.9,1),
+                       limits = c(-0.5, 1.5),
+                       labels = c("Low", "", "", "","", "", "High"))+
+  scale_x_continuous(breaks = c(-3:3), expand = c(0,0))+
+  scale_y_continuous(breaks = c(-3:3), expand = c(0,0))+
+  xlab("Current d")+
+  ylab("Historical d")+
+  #scale_fill_manual(values = c("white", "blue"))+
+  coord_equal()+
+  theme_classic()+
+  theme(axis.text = element_text(size =14), 
+        axis.title = element_text(size = 16), 
+        legend.title.align = 0.05, 
+        legend.title = element_text(size = 16), 
+        legend.text = element_text(size = 14))
+
--- analysis/R/noseq_simulation.R
+++ analysis/R/noseq_simulation.R
@@ -97,30 +97,3 @@ for(j in 1:nrow(sim_result)){
   sim_result$C90[j] = calc_c(theta1$LCL90,theta1$UCL90,sim_result$d[j])
   sim_result$C95[j] = calc_c(theta1$LCL95,theta1$UCL95,sim_result$d[j])
 }
-
-
-
-#### dino plot #####
-ggplot()+
-  geom_tile(data = sims, aes(x = d, y = d_h,
-                             fill = C80),
-            color="black")+
-  facet_grid(h~a)+
-  scale_fill_gradient2(expression(C80),
-                       low = "white", 
-                       high = "blue",
-                       breaks = c(0,0.2,0.4,0.6,0.8,0.9,1),
-                       limits = c(-0.5, 1.5),
-                       labels = c("Low", "", "", "","", "", "High"))+
-  scale_x_continuous(breaks = c(-3:3), expand = c(0,0))+
-  scale_y_continuous(breaks = c(-3:3), expand = c(0,0))+
-  xlab("Current d")+
-  ylab("Historical d")+
-  #scale_fill_manual(values = c("white", "blue"))+
-  coord_equal()+
-  theme_classic()+
-  theme(axis.text = element_text(size =14), 
-        axis.title = element_text(size = 16), 
-        legend.title.align = 0.05, 
-        legend.title = element_text(size = 16), 
-        legend.text = element_text(size = 14))

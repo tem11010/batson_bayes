fba4ac03cc1330ea5f4195f276efa68e2295979a
tem11010 <timothy.e.moore@uconn.edu>
2021-12-03 11:04:49 -0500

    edit coverage plot colors for more detail

--- analysis/R/coverageplot_func.R
+++ analysis/R/coverageplot_func.R
@@ -1,23 +1,28 @@
 coverageplot_func <- function(dat,CI,xlabel,ylabel){
   library(ggplot2)
+  library(viridis)
   cust_labeller_a <- function(x) paste0("a = ", x)
   cust_labeller_h <- function(x) paste0("h = ", x)
+  CI.d <- cut(CI, breaks = 5, dig.lab = 2)
+  levels(CI.d)[1] <-   "(0,0.1]"
   ggplot()+
-    geom_tile(data = dat, aes(x = d, y = d_h,fill = CI),
-              color="black")+
+    geom_tile(data = dat, aes(x = d, y = d_h,fill = CI.d),
+              color="black", alpha = 0.5)+
     facet_grid(h~a, 
                labeller = labeller(
                  h = cust_labeller_h, 
                  a = cust_labeller_a)
               ) +
-    scale_fill_gradient(
-      'coverage', 
-      low = "white",
-      high = "blue", 
-      #breaks = c(0,0.5,0.7,0.9, 1), 
-      #limits = c(0,1),
-      #labels = c("low", "", "medium", "","high")
-      )+
+
+    scale_fill_manual("Bias Detection Rate", values = rev(viridis(5)))+
+    # scale_fill_gradient(
+    #   'coverage', 
+    #   low = "white",
+    #   high = "blue", 
+    #   #breaks = c(0,0.5,0.7,0.9, 1), 
+    #   #limits = c(0,1),
+    #   #labels = c("low", "", "medium", "","high")
+    #   )+
     scale_x_continuous(breaks = c(-3:3), expand = c(0,0))+
     scale_y_continuous(breaks = c(-3:3), expand = c(0,0))+
     xlab(xlabel)+

c01c80dfb1552dbc54c4512f671ef9935e9b21f4
Tim Moore <timothy.e.moore@uconn.edu>
2023-07-28 12:43:34 -0400

    Delete R directory

--- R/render_paper.R
+++
@@ -1,5 +0,0 @@
-# render paper to separate output folder
-
-output_path <- here::here("analysis","_output")
-input_path <- here::here("analysis","paper_draft3.Rmd")
-rmarkdown::render(input = input_path, output_dir = output_path)
